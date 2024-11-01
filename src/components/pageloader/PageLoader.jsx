a83653bfccf7f2e7eef1f22f90596a00f77047c0
Programming-Sai <saiahprog6@gmail..com>
2024-11-01 22:03:36 +0000

    Editted the page loader component

--- src/components/pageloader/PageLoader.jsx
+++ src/components/pageloader/PageLoader.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
-import styles from './pageloader.module.css'
+import React from "react";
+import styles from "./pageloader.module.css";
+import BASE_PATH from "../../../base";
 
 const PageLoader = ({ widthOfSlice, slices, img, isBorderRadius }) => {
-  if (!slices){
+  if (!slices) {
     slices = 10;
   }
 
-  if (!widthOfSlice){
+  if (!widthOfSlice) {
     widthOfSlice = 60;
   }
 
-  if (!img){
-    img = '/favicon.ico';
+  if (!img) {
+    img = "/favicon.ico";
   }
-  
+
   return (
     <div className={styles.container}>
       <div className={styles.box}>
         <div>
-            {Array.from( {length: slices }, (_, i)=>(
-              <span key={i} style={{'--i': i, '--slices': slices, '--img': `url(${img})`, '--width': `${widthOfSlice}px`, borderRadius: isBorderRadius ? '20px' : '0'}}></span>
-            ))}
+          {Array.from({ length: slices }, (_, i) => (
+            <span
+              key={i}
+              style={{
+                "--i": i,
+                "--slices": slices,
+                "--img": `url(${BASE_PATH}${img})`,
+                "--width": `${widthOfSlice}px`,
+                borderRadius: isBorderRadius ? "20px" : "0",
+              }}
+            ></span>
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PageLoader
+export default PageLoader;
