b2c0fdac15fa6eeb5b7d72523ada36abd43422e0
Programming-Sai <saiahprog6@gmail..com>
2024-11-01 10:22:28 +0000

    Adding items to static-deploy branch

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
-import styles from './about.module.css';
-import Wrapper from '@/components/pagewrapper/Wrapper';
-import Navbar from '@/components/navbar/Navbar';
-import Footer from '@/components/footer/Footer';
-import AboutHeroSection from '@/components/aboutherosection/AboutHeroSection';
-import AboutContent from '@/components/aboutcontent/AboutContent';
-import PopularPosts from '@/components/popularposts/PopularPosts';
-
+import React from "react";
+import styles from "./about.module.css";
+import Wrapper from "@/components/pagewrapper/Wrapper";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
+import AboutHeroSection from "@/components/aboutherosection/AboutHeroSection";
+import AboutContent from "@/components/aboutcontent/AboutContent";
+import PopularPosts from "@/components/popularposts/PopularPosts";
 
 const About = ({ slug }) => {
   return (
     <>
-        <Navbar />
-        <Wrapper>
-            <div className={styles.container} style={{zIndex: 1}}>
-                <AboutHeroSection />
-                <div className={styles.flexWrapper}>
-                  <AboutContent className={styles.itemOne} />
-                  <PopularPosts className={styles.itemTwo} glow={ true } borderRad='20px' marginBlock='0%' isOutline='2px'/>
-                </div>
-            </div>
-        </Wrapper>
-        <Footer />
+      <Navbar />
+      <Wrapper>
+        <div className={styles.container} style={{ zIndex: 1 }}>
+          <AboutHeroSection />
+          <div className={styles.flexWrapper}>
+            <AboutContent className={styles.itemOne} />
+            <PopularPosts
+              className={styles.itemTwo}
+              glow={true}
+              borderRad="20px"
+              marginBlock="0%"
+              isOutline="2px"
+            />
+          </div>
+        </div>
+      </Wrapper>
+      <Footer />
     </>
+  );
+};
 
-  )
-
-}
-
-export default About
+export default About;
--- src/components/editorone/EditorOne.jsx
+++ src/components/editorone/EditorOne.jsx
@@ -1,89 +1,98 @@
-import React, { useContext, useState, useEffect } from 'react';
-import Quill from 'quill';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';  
-import 'react-quill/dist/quill.bubble.css';  
-import styles from './editorone.module.css';
-
-
+import React, { useContext, useState, useEffect } from "react";
+import Quill from "quill";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "react-quill/dist/quill.bubble.css";
+import styles from "./editorone.module.css";
 
 const EditorOne = ({ blogContent, setBlogContent, quillTheme }) => {
-
-
-
   const modules = {
     toolbar: [
-      [{ 'font': [] }],
-      [{ 'size': ['small', false, 'large', 'huge'] }],
-      [{ 'header': [1, 2, 3, 4, 5, false] }],
-      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-      [{ 'indent': '-1' }, { 'indent': '+1' }],
-      [{ 'align': [] }],
-      ['bold', 'italic', 'underline', 'strike'],
-      [{ 'color': [] }, { 'background': [] }],
-      ['link', 'image', 'clean', 'code-block', 'blockquote', 'video' ], 
+      [{ font: [] }],
+      [{ size: ["small", false, "large", "huge"] }],
+      [{ header: [1, 2, 3, 4, 5, false] }],
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ indent: "-1" }, { indent: "+1" }],
+      [{ align: [] }],
+      ["bold", "italic", "underline", "strike"],
+      [{ color: [] }, { background: [] }],
+      ["link", "image", "clean", "code-block", "blockquote", "video"],
     ],
     clipboard: {
       matchVisual: false,
     },
   };
-  
 
   const formats = [
-    'header', 'font', 'size', 'bold', 'italic', 'underline', 'strike', 
-    'color', 'background', 'list', 'bullet', 'indent', 'align', 
-    'link', 'image', 'blockquote', 'code-block', 'video',
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "color",
+    "background",
+    "list",
+    "bullet",
+    "indent",
+    "align",
+    "link",
+    "image",
+    "blockquote",
+    "code-block",
+    "video",
   ];
 
-
   useEffect(() => {
-    const adjustBubblePosition = () => {
-      const tooltip = document.querySelector('.ql-bubble .ql-tooltip');
-      if (!tooltip) return;
-  
-      const rect = tooltip.getBoundingClientRect();
-      const screenWidth = window.innerWidth;
-      const screenHeight = window.innerHeight;
-  
-      // Adjust horizontal overflow
-      if (rect.right > screenWidth) {
-        tooltip.style.left = `${screenWidth - rect.width - 10}px`;  // Adjust for overflow on the right
-      }
-      if (rect.left < 0) {
-        // tooltip.style.right = `20px`;  
-        tooltip.style.right = `${screenWidth - rect.width - 10}px`;  // Adjust for overflow on the right
-      }
-  
-      // Adjust vertical overflow
-      if (rect.bottom > screenHeight) {
-        tooltip.style.top = `${screenHeight - rect.height - 10}px`;  // Adjust for overflow on the bottom
-      }
-      if (rect.top < 0) {
-        tooltip.style.top = `10px`;  // Adjust for overflow on the top
-      }
-    };
-  
-    // Adjust when the tooltip appears
-    document.addEventListener('mousemove', adjustBubblePosition);
-  
-    return () => {
-      document.removeEventListener('mousemove', adjustBubblePosition);
-    };
+    if (typeof document !== "undefined") {
+      const adjustBubblePosition = () => {
+        const tooltip = document.querySelector(".ql-bubble .ql-tooltip");
+        if (!tooltip) return;
+
+        const rect = tooltip.getBoundingClientRect();
+        const screenWidth = window.innerWidth;
+        const screenHeight = window.innerHeight;
+
+        // Adjust horizontal overflow
+        if (rect.right > screenWidth) {
+          tooltip.style.left = `${screenWidth - rect.width - 10}px`; // Adjust for overflow on the right
+        }
+        if (rect.left < 0) {
+          // tooltip.style.right = `20px`;
+          tooltip.style.right = `${screenWidth - rect.width - 10}px`; // Adjust for overflow on the right
+        }
+
+        // Adjust vertical overflow
+        if (rect.bottom > screenHeight) {
+          tooltip.style.top = `${screenHeight - rect.height - 10}px`; // Adjust for overflow on the bottom
+        }
+        if (rect.top < 0) {
+          tooltip.style.top = `10px`; // Adjust for overflow on the top
+        }
+      };
+
+      // Adjust when the tooltip appears
+      document.addEventListener("mousemove", adjustBubblePosition);
+
+      return () => {
+        document.removeEventListener("mousemove", adjustBubblePosition);
+      };
+    }
   }, []);
-  
 
   return (
     <div className={styles.blogContent} key={quillTheme}>
-            <ReactQuill 
-                value={blogContent} 
-                onChange={setBlogContent} 
-                modules={modules}
-                formats={formats}
-                theme={quillTheme}
-                placeholder="Blog Content..."
-            />
-        </div>
-  )
-}
+      <ReactQuill
+        value={blogContent}
+        onChange={setBlogContent}
+        modules={modules}
+        formats={formats}
+        theme={quillTheme}
+        placeholder="Blog Content..."
+      />
+    </div>
+  );
+};
 
-export default EditorOne
+export default EditorOne;
