8871dc9bbdc0f0747a5486f737a9720a69842acc
Programming-Sai <saiahprog6@gmail..com>
2024-11-01 18:18:24 +0000

    Modifying config file again

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-// Import the base path from the basePath.js file
-const BASE_PATH = require("./basePath");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compress: true,
   output: "export",
-  basePath: BASE_PATH, // Use the imported base path
-  assetPrefix: `${BASE_PATH}/`,
+  basePath: "/Blog",
+  assetPrefix: "/Blog/",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
