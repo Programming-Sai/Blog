7c963f8d8325a56f62427975a3fd526a486e65a6
Programming-Sai <saiahprog6@gmail.com>
2025-01-19 16:07:46 +0000

    Fixed some issues in the configuration. now some useful data is available from the api.

--- src/app/api/analytics/route.js
+++ src/app/api/analytics/route.js
@@ -14,30 +14,120 @@ const analyticsDataClient = new BetaAnalyticsDataClient({
 
 export const GET = async () => {
   try {
-    const [response] = await analyticsDataClient.runReport({
+    // Today's Visits: date, userType, country
+    const [todayVisits] = await analyticsDataClient.runReport({
       property: `properties/${propertyId}`,
       dateRanges: [
         {
-          startDate: `7daysAgo`, // e.g., "7daysAgo" or "30daysAgo"
+          startDate: "today",
           endDate: "today",
         },
       ],
-      dimensions: [
-        {
-          name: "year", // Data grouped by year
-        },
-      ],
-      metrics: [
-        {
-          name: "activeUsers", // Returns the active users
-        },
-      ],
+      dimensions: [{ name: "date" }],
+      metrics: [{ name: "activeUsers" }],
     });
 
-    return NextResponse.json({ response }, { status: 200 });
+    // % Unique Visits: deviceCategory, source/medium
+    const [uniqueVisits] = await analyticsDataClient.runReport({
+      property: `properties/${propertyId}`,
+      dateRanges: [
+        {
+          startDate: "2025-01-18",
+          endDate: "today",
+        },
+      ],
+      dimensions: [{ name: "date" }],
+      metrics: [{ name: "activeUsers" }],
+    });
+
+    // Bounce Rate: pagePath, deviceCategory
+    const [bounceRate] = await analyticsDataClient.runReport({
+      property: `properties/${propertyId}`,
+      dateRanges: [
+        {
+          startDate: "2025-01-18",
+          endDate: "today",
+        },
+      ],
+      dimensions: [{ name: "pagePath" }, { name: "deviceCategory" }],
+      metrics: [{ name: "bounceRate" }],
+    });
+
+    // Traffic Sources Chart: source/medium, region
+    const [trafficSources] = await analyticsDataClient.runReport({
+      property: `properties/${propertyId}`,
+      dateRanges: [
+        {
+          startDate: "2025-01-18",
+          endDate: "today",
+        },
+      ],
+      dimensions: [{ name: "sourceMedium" }],
+      metrics: [{ name: "activeUsers" }],
+    });
+
+    ////// MIGHT HAVE TO REMOVE THIS---------------------
+
+    // // Content Engagement Chart: pagePath, engagementType
+    // const [contentEngagement] = await analyticsDataClient.runReport({
+    //   property: `properties/${propertyId}`,
+    //   dateRanges: [
+    //     {
+    //       //   startDate: "2025-01-18",
+    //       startDate: "30daysago",
+    //       endDate: "today",
+    //     },
+    //   ],
+    //   dimensions: [{ name: "pagePath" }, { name: "engagementType" }],
+    //   metrics: [{ name: "engagedSessions" }],
+    // });
+
+    ////// MIGHT HAVE TO REMOVE THIS---------------------
+
+    // // Growth Rate Chart: week, month
+    // const [growthRate] = await analyticsDataClient.runReport({
+    //   property: `properties/${propertyId}`,
+    //   dateRanges: [
+    //     {
+    //       startDate: "2025-01-18",
+    //       endDate: "today",
+    //     },
+    //   ],
+    //   dimensions: [{ name: "week" }, { name: "month" }],
+    //   metrics: [{ name: "activeUsers" }],
+    // });
+
+    // // SEO Metrics: pagePath, searchEngine, keyword
+    // const [seoMetrics] = await analyticsDataClient.runReport({
+    //   property: `properties/${propertyId}`,
+    //   dateRanges: [
+    //     {
+    //       startDate: "2025-01-18",
+    //       endDate: "today",
+    //     },
+    //   ],
+    //   dimensions: [
+    //     // { name: "pagePath" },
+    //     // { name: "searchEngine" },
+    //     // { name: "keyword" },
+    //   ],
+    //   metrics: [{ name: "entrances" }],
+    // });
+
+    // Combining all the results
+    const result = {
+      todayVisits,
+      uniqueVisits,
+      bounceRate,
+      trafficSources,
+      //   contentEngagement,
+      //   growthRate,
+      //   seoMetrics,
+    };
+
+    return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     console.error("Error fetching Analytics data:", error);
-
     return NextResponse.json(
       { error: `Failed to fetch Analytics data: ${error.message}` },
       { status: 500 }
